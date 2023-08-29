e904bfd0c698025775777ed22a3f8143ca40d1d5
heros Avidisian <heros.avidisian@iths.se>
2023-08-29 17:55:48 +0200

    la till meny för elpriser

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,19 @@
 package org.example;
-
+import java.util.Objects;
+import java.util.Scanner;
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+    Scanner sc = new Scanner(System.in);
+    String meny = """
+            Elpriser
+            ==============
+            1. Inmatning
+            2. Min, Max och Medel
+            3. Sortera
+            4. Bästa Laddningstid (4h)
+            5. Visualisering
+            e. Avsluta
+            """;
+        System.out.println(meny);
     }
 }
