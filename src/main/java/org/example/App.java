1efe2871a4089c584e3abbec00b4a788f6aa94d2
heros Avidisian <heros.avidisian@iths.se>
2023-08-31 13:54:58 +0200

    la till meny för elpriser

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,23 +2,29 @@ package org.example;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String meny = """
-            Elpriser
-            ==============
-            1. Inmatning
-            2. Min, Max och Medel
-            3. Sortera
-            4. Bästa Laddningstid (4h)
-            5. Visualisering
-            e. Avsluta
-            """;
+        Scanner sc = new Scanner(System.in);
+        int[] inputs = new int[24];
+        String meny = """
+                Elpriser
+                ==============
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                5. Visualisering
+                e. Avsluta
+                """;
         System.out.println(meny);
         int option1 = sc.nextInt();
-        if () {
-            System.out.println("Elpriser under 24h");
-        } else if () {
-            
+        int option2 = sc.nextInt();
+        if (option1 == 1) {
+            System.out.println("Skriv in elpriser under det senaste dygnet");
+            for (int i = 0; i < 24; i++)
+                inputs[i] = sc.nextInt();
         }
+        else if (option2==2)
+
+
+
     }
 }
