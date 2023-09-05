4f2ba76d267246a7d0d87764963f0f75a278b88f
heros Avidisian <heros.avidisian@iths.se>
2023-09-05 18:07:58 +0200

    la till 2 nya case.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,25 +1,49 @@
 package org.example;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] inputs = new int[24];
-        String meny = """
-                Elpriser
-                ==============
-                1. Inmatning
-                2. Min, Max och Medel
-                3. Sortera
-                4. Bästa Laddningstid (4h)
-                5. Visualisering
-                e. Avsluta
-                """;
-        System.out.println(meny);
-        int option1 = sc.nextInt();
-        if (option1 == 1) {
-            System.out.println("Skriv in elpriser under det senaste dygnet");
-            for (int i = 0; i < 24; i++)
-                inputs[i] = sc.nextInt();
+        int[] electricPrices = new int[24];
+        boolean running = true;
+
+        while (running) {
+            String meny = """
+                    Elpriser
+                    =======
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    e. Avsluta
+                    """;
+            System.out.println(meny);
+            System.out.println("Välj ett alternativ: ");
+            String Choice = sc.next();
+
+            switch (Choice) {
+                case "1":
+                    for (int i = 0; i < 24; i++) {
+                        System.out.println("Ange elpriset för timme " + i + "-" + (i + 1) + " ( kr per kw/h): ");
+                        try {
+                            electricPrices[i] = sc.nextInt();
+                        } catch (InputMismatchException e) {
+                            System.out.println("ogiltig inmatning. ange ett heltal.");
+                            sc.next();
+                            i--;
+                        }
+                    }
+                    break;
+                case "2":
+                    int minPris = Arrays.stream(electricPrices).min().orElse(0);
+                    int maxPris = Arrays.stream(electricPrices).max().orElse(0);
+                    double medelPris = Arrays.stream(electricPrices).average().orElse(0);
+                    System.out.println("Minsta pris: " + minPris + " kr per kW/h");
+                    System.out.println("Största pris: " + maxPris + " kr per kW/h");
+                    System.out.println("Medelpris: " + medelPris + " kr per kW/h");
+                    break;
+            }
         }
     }
 }
