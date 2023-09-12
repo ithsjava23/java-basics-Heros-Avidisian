f740adfa7e2b5f33cda5b611c7d0847b29637d8b
heros Avidisian <heros.avidisian@iths.se>
2023-09-12 21:48:23 +0200

    snart klar med alla tester 1 kvar

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Locale swedishLocale = new Locale("sv", "SE");
-        Locale.setDefault(swedishLocale);
+        Locale.setDefault(Locale.of("sv", "SE"));
         Scanner sc = new Scanner(System.in);
         double[] electricPrices = new double[24];
 
@@ -34,6 +35,8 @@ public class App {
                 case '2':
                     CalculateMinMaxAverage(electricPrices);
                     break;
+                case '3':
+                    sortPricesHighToLow(electricPrices);
 
                 case 'e', 'E':
                     break;
@@ -42,6 +45,22 @@ public class App {
                     break;
             }
         } while (Choice != 'e' && Choice != 'E');
+    }
+
+    private static void sortPricesHighToLow(double[] electricPrices) {
+        if (electricPrices[0] == 0) {
+            System.out.print("Du måste först mata in elpriser\n");
+        }else {
+            HourPricePair[] hourPricePairs = new HourPricePair[24];
+            for (int i = 0; i < 24; i++) {
+                hourPricePairs[i] = new HourPricePair(i, (int) electricPrices[i]);
+            }
+            Arrays.sort(hourPricePairs);
+            System.out.print("Timmar sorterad efter pris (Dyrast till billigast)\n");
+            for (HourPricePair pair : hourPricePairs){
+                System.out.print(pair + "\n");
+            }
+        }
     }
 
     private static void CalculateMinMaxAverage(double[] electricPrices) {
@@ -62,7 +81,10 @@ public class App {
         }
 
         double averagePrice = Arrays.stream(electricPrices).average().orElse(0);
-
+        DecimalFormatSymbols customSymbols = new DecimalFormatSymbols(Locale.GERMAN);
+        customSymbols.setDecimalSeparator(',');
+        DecimalFormat df = new DecimalFormat("#,##0.##", customSymbols);
+        DecimalFormat dF = new DecimalFormat("#,##0.00", customSymbols);
         minPrice = Math.round(minPrice * 100.0) / 100.0;
         maxPrice = Math.round(maxPrice * 100.0) / 100.0;
         averagePrice = Math.round(averagePrice * 100.0) / 100.0;
@@ -73,17 +95,17 @@ public class App {
             String maxStartTime = (maxHour < 10) ? "0" + maxHour : String.valueOf(maxHour);
             String maxEndTime = ((maxHour + 1) % 24 < 10) ? "0" + (maxHour + 1) % 24 : String.valueOf((maxHour + 1) % 24);
 
-            System.out.print("Lägsta pris: " + minStartTime + "-" + minEndTime + ", " + minPrice + " öre/kwh\n");
-            System.out.print("Högsta pris: " + maxStartTime + "-" + maxEndTime + ", " + maxPrice + " öre/kwh\n");
+            System.out.printf("Lägsta pris: " + minStartTime + "-" + minEndTime + ", " + df.format(minPrice) + " öre/kWh\n");
+            System.out.printf("Högsta pris: " + maxStartTime + "-" + maxEndTime + ", " + df.format(maxPrice) + " öre/kWh\n");
         }
 
-        System.out.print("Medelpris: " + averagePrice + " öre/kWh\n");
+        System.out.printf("Medelpris: " + dF.format(averagePrice) + " öre/kWh\n");
     }
 
     private static void getUserInput(Scanner sc, double[] electricPrices) {
         for (int i = 0; i < 24; i++) {
             int startHour = i;
-            int endHour = (i + 1) % 24;
+            int endHour = (i + 1) % 25;
             String startTime = (startHour < 10) ? "0" + startHour : String.valueOf(startHour);
             String endTime = (endHour < 10) ? "0" + endHour : String.valueOf(endHour);
             System.out.print("Ange elpriset för timme " + startTime + "-" + endTime + " ( kr per kw/h): \n");
@@ -94,6 +116,23 @@ public class App {
                 sc.nextLine();
                 i--;
             }
+        }
+    }
+    static class HourPricePair implements Comparable<HourPricePair>{
+        private final int hour;
+        private final int price;
+
+        public HourPricePair(int hour, int price){
+            this.hour = hour;
+            this.price = price;
+        }
+        public int compareTo(HourPricePair other) {
+            return Double.compare(other.price, this.price);
+        }
+        public String toString() {
+            String startTime = (hour < 10) ? "0" + hour : String.valueOf(hour);
+            String endTime = (((hour + 1) % 25)<10) ? "0" + ((hour + 1) % 24) : String.valueOf((hour + 1) % 25);
+            return startTime + "-" + endTime + " " + price + " öre";
         }
     }
 }
